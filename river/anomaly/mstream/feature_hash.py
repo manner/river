6c5787245a7502a68f57271893df1fe2bf4fd258
Moritz Manner <moritz.manner@gmail.com>
2022-05-02 14:01:21 +0200

    Implement first pass of FeatureHash inserting

--- river/anomaly/mstream/feature_hash.py
+++ river/anomaly/mstream/feature_hash.py
@@ -1,3 +1,4 @@
+from math import floor, log10
 import random
 
 
@@ -34,8 +35,40 @@ class FeatureCategorialHash():
 
 
 class FeatureNumericalHash():
-    def __init__(self):
-        pass
+    def __init__(self, number_buckets, number_features):
+        self.number_buckets = number_buckets
+        self.number_features = number_features
+        self.count = [[0 for i in range(number_buckets)] for j in range(number_features)]
+        self.total_count = [[0 for i in range(number_buckets)] for j in range(number_features)]
 
     def insert(self, x):
-        pass
+
+        first = True
+        for i, feature in enumerate(x):
+            current_feature = log10(1 + feature)
+            if first:
+                first = False
+                min = current_feature
+                max = current_feature
+                current_feature = 0
+            else:
+                min = min(min, current_feature)
+                max = max(max, current_feature)
+                if(min == max):
+                    current_feature = 0
+                else:
+                    current_feature = self.normalize(current_feature, min, max)
+
+            bucket = hash(current_feature)
+            self.count[i][bucket] += 1
+            self.total_count[i][bucket] += 1
+
+    def get_count(self, x):
+        bucket = self.hash(x)
+        return self.count[bucket]
+
+    def hash(self, value):
+        return floor(value * self.number_buckets) % self.number_buckets
+
+    def normalize(self, value, min, max):
+        return (value - min) / (max - min)
