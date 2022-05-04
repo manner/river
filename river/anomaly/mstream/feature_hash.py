457d702f2a57d779eae9be260b86c36c587bcac0
Moritz Manner <moritz.manner@gmail.com>
2022-05-04 14:47:28 +0200

    Finish FeatureNumericalHash

--- river/anomaly/mstream/feature_hash.py
+++ river/anomaly/mstream/feature_hash.py
@@ -38,8 +38,7 @@ class FeatureNumericalHash():
     def __init__(self, number_buckets, number_features):
         self.number_buckets = number_buckets
         self.number_features = number_features
-        self.count = [[0 for i in range(number_buckets)] for j in range(number_features)]
-        self.total_count = [[0 for i in range(number_buckets)] for j in range(number_features)]
+        self.clear()
 
     def insert(self, x):
 
@@ -63,12 +62,33 @@ class FeatureNumericalHash():
             self.count[i][bucket] += 1
             self.total_count[i][bucket] += 1
 
-    def get_count(self, x):
-        bucket = self.hash(x)
-        return self.count[bucket]
+    def get_count(self, x, t):
+        result = 0
+        for feature in x:
+            bucket = self.hash(feature)
+            result += self.counts_to_anom(self.total_count[bucket], self.count[bucket], t)
+        return result
 
     def hash(self, value):
+        # this is currently more simplified than the cpp implementation
         return floor(value * self.number_buckets) % self.number_buckets
 
     def normalize(self, value, min, max):
         return (value - min) / (max - min)
+
+    def clear(self):
+        self.count = [[0 for i in range(self.number_buckets)] for j in range(self.number_features)]
+        self.total_count = [[0 for i in range(self.number_buckets)]
+                            for j in range(self.number_features)]
+
+    def lower(self, factor):
+        for i in range(self.number_features):
+            for j in range(self.number_buckets):
+                self.count[i][j] *= factor
+
+    # This is directly copied from the cpp implementation
+    # I don't know exactly where this function is coming from, as it's not in the paper
+    def counts_to_anom(self, tot, cur, cur_t):
+        cur_mean = tot / cur_t
+        sqerr = pow(max(0, cur - cur_mean), 2)
+        return sqerr / cur_mean + sqerr / (cur_mean * max(1, cur_t - 1))
