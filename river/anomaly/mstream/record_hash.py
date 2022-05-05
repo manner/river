541ebb308468b9229b7b9af54d503179e583cc98
Paola Ortega Saborio <paola@saborio.org>
2022-05-05 20:12:14 +0200

    Update record_hash.py

--- river/anomaly/mstream/record_hash.py
+++ river/anomaly/mstream/record_hash.py
@@ -1,6 +1,7 @@
 import math
 import random
 from itertools import compress
+import numpy as np
 
 class RecordHash():
     def __init__(self, is_categorical, number_buckets, dimension_1, dimension_2, seed):
@@ -50,8 +51,20 @@ class RecordCategorialHash():
         return resid + (0 if resid < 0 else self.num_buckets)
 
 class RecordNumericalHash():
-    def __init__(self):
-        pass
+    def __init__(self, dimension_1, num_buckets):
+        self.dimension_1 = dimension_1
+        self.num_buckets = num_buckets
+        self.k = math.ceil(math.log2(self.num_buckets))
+        p = dimension_1
+        self.rand_vectors = np.random.normal(0, 1, size=(self.k, p))
 
-    def hash(self):
-        return 0
+    def hash(self, x_numerical):
+        x_numerical_vector = np.array(x_numerical)
+        bitset = ""
+        for i in range(0, self.k):
+            dot_product = np.dot(x_numerical_vector, self.rand_vectors[i].T)
+            if dot_product > 0:
+                bitset += "1"
+            else:
+                bitset += "0"
+        return int(bitset, 2)
