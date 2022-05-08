eee31bb71f57da914dada89574c19b07ca39cc67
Moritz Manner <moritz.manner@gmail.com>
2022-05-09 00:54:19 +0200

    Review suggestions

--- river/anomaly/mstream/record_hash.py
+++ river/anomaly/mstream/record_hash.py
@@ -10,10 +10,12 @@ class RecordHash():
         self.is_numerical = [not feature for feature in is_categorical]
         # self.num_rows = num_rows number of hash functions, we will just use 2 for now
         self.number_buckets = number_buckets
-        self.categorical_hash = RecordCategorialHash()
-        self.numerical_hash = RecordNumericalHash()
-        self.dimension_1 = len(self.is_numerical)  # number of numerical features
-        self.dimension_2 = len(self.is_categorical)  # number of categorical features
+        self.number_numerical_features = len(self.is_numerical)
+        self.number_categorical_features = len(self.is_categorical)
+        self.numerical_hash = RecordNumericalHash(
+            self.number_buckets, self.number_numerical_features)
+        self.categorical_hash = RecordCategorialHash(
+            self.number_buckets, self.number_categorical_features)
 
     def insert(self, x):
         # divide x into categorical and numerical
@@ -27,17 +29,17 @@ class RecordHash():
 
 
 class RecordCategorialHash():
-    def __init__(self, dimension_2, num_buckets):
+    def __init__(self, num_buckets, number_categorical_features):
         self.cat_recordhash = []
-        self.dimension_2 = dimension_2
+        self.number_categorical_features = number_categorical_features
         self.num_buckets = num_buckets
-        for i in range(self.dimension_2):
+        for i in range(self.number_categorical_features):
             self.cat_recordhash.append({})
         self.clear()
 
     def hash(self, x_categorical):
         resid = 0
-        for i in range(self.dimension_2):
+        for i in range(self.number_categorical_features):
             current_category = self.cat_recordhash[i]
             key = hash(x_categorical[i])
             if current_category[key]:
@@ -46,7 +48,7 @@ class RecordCategorialHash():
                 current_category[key] = 1
             value = current_category[key]
             resid = (resid + key * value) % self.num_buckets
-        self.bucket_cat = resid + (0 if resid < 0 else self.num_buckets)
+        self.bucket_cat = resid + (self.num_buckets if resid < 0 else 0)
 
     def clear(self):
         self.bucket_cat = 0
@@ -56,11 +58,11 @@ class RecordCategorialHash():
 
 
 class RecordNumericalHash():
-    def __init__(self, dimension_1, num_buckets):
-        self.dimension_1 = dimension_1
+    def __init__(self, num_buckets, number_numerical_features):
+        self.number_numerical_features = number_numerical_features
         self.num_buckets = num_buckets
         self.k = math.ceil(math.log2(self.num_buckets))
-        p = dimension_1
+        p = number_numerical_features
         self.rand_vectors = np.random.normal(0, 1, size=(self.k, p))
         self.clear()
 
