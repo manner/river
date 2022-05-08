acbbdc0b652ce4be9826a1636f3bc2aa6b3514f0
Moritz Manner <moritz.manner@gmail.com>
2022-05-09 00:45:50 +0200

    Format and simplify range

--- river/anomaly/mstream/record_hash.py
+++ river/anomaly/mstream/record_hash.py
@@ -3,39 +3,41 @@ import random
 from itertools import compress
 import numpy as np
 
+
 class RecordHash():
     def __init__(self, is_categorical, number_buckets):
         self.is_categorical = is_categorical
-        self.is_numerical = [ not feature for feature in is_categorical ]
+        self.is_numerical = [not feature for feature in is_categorical]
         # self.num_rows = num_rows number of hash functions, we will just use 2 for now
         self.number_buckets = number_buckets
         self.categorical_hash = RecordCategorialHash()
         self.numerical_hash = RecordNumericalHash()
-        self.dimension_1 = len(self.is_numerical) # number of numerical features
-        self.dimension_2 = len(self.is_categorical) # number of categorical features
+        self.dimension_1 = len(self.is_numerical)  # number of numerical features
+        self.dimension_2 = len(self.is_categorical)  # number of categorical features
 
     def insert(self, x):
         # divide x into categorical and numerical
         x_categorical = compress(x, self.is_categorical)
         x_numerical = compress(x, self.is_numerical)
         self.categorical_hash.hash(x_categorical)
-        self.numerical_hash.hash(x_numerical)    
+        self.numerical_hash.hash(x_numerical)
 
     def get_count(self):
         return (self.categorical_hash.get_count(), self.numerical_hash.get_count()) % self.number_buckets
+
 
 class RecordCategorialHash():
     def __init__(self, dimension_2, num_buckets):
         self.cat_recordhash = []
         self.dimension_2 = dimension_2
         self.num_buckets = num_buckets
-        for i in range(0, self.dimension_2):
+        for i in range(self.dimension_2):
             self.cat_recordhash.append({})
         self.clear()
 
     def hash(self, x_categorical):
         resid = 0
-        for i in range(0, self.dimension_2):
+        for i in range(self.dimension_2):
             current_category = self.cat_recordhash[i]
             key = hash(x_categorical[i])
             if current_category[key]:
@@ -52,6 +54,7 @@ class RecordCategorialHash():
     def get_count(self):
         return self.bucket_cat
 
+
 class RecordNumericalHash():
     def __init__(self, dimension_1, num_buckets):
         self.dimension_1 = dimension_1
@@ -64,7 +67,7 @@ class RecordNumericalHash():
     def hash(self, x_numerical):
         x_numerical_vector = np.array(x_numerical)
         bitset = ""
-        for i in range(0, self.k):
+        for i in range(self.k):
             dot_product = np.dot(x_numerical_vector, self.rand_vectors[i].T)
             if dot_product > 0:
                 bitset += "1"
