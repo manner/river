8c1b4d19c646e401bb6b7b881584c97eef84e466
Moritz Manner <moritz.manner@gmail.com>
2022-05-17 18:02:30 +0200

    Add 10k test dataset

--- river/mstream_test.py
+++ river/mstream_test.py
@@ -21,12 +21,14 @@ for i in range(0, unswcateg_size):
 headers.append("timestamp")
 dataset = "elec"
 
+
 def print_progress(sample_id, acc, kappa):
     print(f'Samples processed: {sample_id}')
     print(f'Accuracy: {acc}')
     print(f'Kappa: {kappa}')
 
-def evaluate(stream, model, n_wait=1000, verbose=False):
+
+def evaluate(stream, model, n_wait=1000, verbose=True):
     acc = metrics.Accuracy()
     acc_rolling = metrics.Rolling(metric=metrics.Accuracy(), window_size=n_wait)
     kappa = metrics.CohenKappa()
@@ -45,19 +47,21 @@ def evaluate(stream, model, n_wait=1000, verbose=False):
         if i % n_wait == 0 and i > 0:
             if verbose:
                 print_progress(i, acc, kappa)
-            raw_results.append([model_name, i, acc.get(), acc_rolling.get(), kappa.get(), kappa_rolling.get()])
+            raw_results.append([model_name, i, acc.get(), acc_rolling.get(),
+                               kappa.get(), kappa_rolling.get()])
         # Learn (train)
         model.learn_one(x)
     print_progress(i, acc, kappa)
     return pd.DataFrame(raw_results, columns=['model', 'id', 'acc', 'acc_roll', 'kappa', 'kappa_roll'])
 
+
 # Load the data, will be transformed into a stream later
-df_categ = pd.read_csv("./test_data/unswcateg.txt", header=None)
-df_numeric = pd.read_csv("./test_data/unswnumeric.txt", header=None)
-df_time = pd.read_csv("./test_data/unswtime.txt", header=None)
+df_categ = pd.read_csv("./test_data_small/unswcateg_10k.txt", header=None)
+df_numeric = pd.read_csv("./test_data_small/unswnumeric_10k.txt", header=None)
+df_time = pd.read_csv("./test_data_small/unswtime_10k.txt", header=None)
 X = pd.concat([df_numeric, df_categ, df_time], axis=1)
 X.columns = headers
-Y = pd.read_csv("./test_data/unsw_label.txt")
+Y = pd.read_csv("./test_data_small/unsw_label_10k.txt")
 
 # Initialize models
 mstream = MStream(feature_types)
